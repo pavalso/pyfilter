8db0e5b383ba7b9bb803dfb7f6bef4f1efc05928
pavalso <92272697+pavalso@users.noreply.github.com>
2023-05-21 16:43:36 +0200

    Minimized error surface

--- app.py
+++ app.py
@@ -2,41 +2,39 @@ import pyfilter
 import flask
 import os
 
-from werkzeug.routing import PathConverter
-
-
-class SanitizedPathConverter(PathConverter):
-
-    def to_python(self, value):
-        abs_path = os.path.abspath(value)
-
-        if not pyfilter.Paths.contains(abs_path):
-            flask.abort(flask.redirect('/'))
-
-        return abs_path
 
 app = flask.Flask(__name__)
-app.url_map.converters['Path'] = SanitizedPathConverter
+
+app.url_map.converters['path'].to_python = lambda _, value: pyfilter.Paths.relative(value)
 
 @app.get('/', defaults = {'path': '.'})
-@app.get('/<Path:path>/')
+@app.get('/<path:path>/')
 def get_dir(path):
     if not os.path.isdir(path):
         flask.abort(404)
 
+    index_path = os.path.join(os.path.abspath(path), 'index.html')
+
+    if os.path.isfile(index_path):
+        return get_file(index_path)
+
+    relative = os.path.relpath(path)
+    relative = '' if relative == '.' else '%s%c' % (relative.replace(os.path.sep, os.path.altsep), os.path.altsep)
+
     walk = next(os.walk(path, onerror=lambda _: flask.abort(403)))
     return flask.render_template(
         'index.html',
+        path = relative,
         dirs = walk[1],
         files = walk[2])
 
-@app.get('/<Path:path>')
+@app.get('/<path:path>')
 def get_file(path):
     if not os.path.isfile(path):
         flask.abort(404)
 
     try:
-        return flask.send_file(path, mimetype='text/plain')
+        return flask.send_file(path)
     except PermissionError:
         flask.abort(403)
 
