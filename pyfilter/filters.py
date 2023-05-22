066a7b3ca7e601c34eb661414bad3c8233a922d1
pavalso <92272697+pavalso@users.noreply.github.com>
2023-05-23 00:40:15 +0200

    Fixed miss behaviours

--- pyfilter/filters.py
+++ pyfilter/filters.py
@@ -8,11 +8,14 @@ class Paths:
 
     @staticmethod
     def contains(path: os.PathLike | str, root: os.PathLike | str = ''):
-        pathd, bpath = os.path.splitdrive(os.path.abspath(path))
         rootd, broot = os.path.splitdrive(os.path.abspath(root))
+        pathd, bpath = os.path.splitdrive(os.path.abspath(os.path.join(root, path)))
         brelative = os.path.normpath(os.path.join(broot, os.path.relpath(bpath or os.path.sep, broot)))
-        _, real = os.path.splitdrive(brelative)
-        return pathd.casefold() == rootd.casefold() and os.path.isabs(broot) == os.path.isabs(real) and os.path.commonpath([broot, real]).startswith(broot)
+
+        if pathd.casefold() != rootd.casefold() or os.path.isabs(broot) != os.path.isabs(brelative) or not os.path.commonpath([broot, brelative]).startswith(broot):
+            return None
+
+        return os.path.join(rootd, brelative)
 
     @staticmethod
     def traversal_filter(path: os.PathLike | str):
